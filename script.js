cea9dbe0b5e9016e2e3f5b253c22d87398af8ab4
Tanu verma <tanikshaverma966@gmail.com>
2024-12-21 16:06:16 +0530

    bottle project

---
+++ script.js
@@ -0,0 +1,31 @@
+//let arr = []
+//n = 5;
+//for (let i = 0; i < n; i++) {
+//  arr[i] = prompt("enter the number")
+//}
+//console.log(arr);//
+// let arr = []
+// max = 0;
+// indx = 0;
+// for (let i = 0; i < n; i++);
+// if (arr[i] > max) {
+//     max = arr[i];
+//     indx = i;
+// }
+
+// console.log("enter the value of array" = max);
+// console.log("enter the value of index" = indx);
+
+
+gsap.from(".animation", {
+    y: -500,
+    duration: 2,
+    delay: 1,
+    rotate: -360,
+})
+
+// gsap.from(" h1 image", {
+//     x: 500,
+//     duration: 2,
+//     delay: 2,
+// })
